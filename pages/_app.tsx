356c34c8221a0441d0090cf1d0be8d13885b41c3
sasidharank <sasiddharan@gmail.com>
2021-09-24 23:48:26 +0530

    feat: added apollo client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+
+import { ApolloProvider } from '@apollo/client'
+import client from '@GraphQL/apollo-client'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,46 @@
-import Head from "next/head";
-import Image from "next/image";
+import { gql } from '@apollo/client'
+import client from '@GraphQL/apollo-client'
 
-export default function Home() {
-  return <div>Hello there!</div>;
+export default function Home({ countries }: { countries: any }) {
+  return (
+    <div className="container h-screen mx-auto">
+      <div className="my-20 text-center">
+        <h1 className="mb-5 text-4xl font-bold">Welcome</h1>
+        <p>The following data is fetched from a external gql endpoint.</p>
+      </div>
+      <div className="grid grid-cols-1 gap-5 lg:grid-cols-3">
+        {countries.map((country: any) => (
+          <div
+            key={country.code}
+            className="p-4 text-center bg-gray-100 rounded-lg"
+          >
+            <h3>{country.name}</h3>
+            <p>
+              {country.code} - {country.emoji}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export async function getServerSideProps() {
+  const { data } = await client.query({
+    query: gql`
+      query Countries {
+        countries {
+          code
+          name
+          emoji
+        }
+      }
+    `
+  })
+
+  return {
+    props: {
+      countries: data.countries.slice(0, 9)
+    }
+  }
 }
